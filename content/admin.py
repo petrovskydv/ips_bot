bdac07e6d9f4475f6572f4fbd15893a5cdc21a8e
Денис Петровский <petrovsky.dv@gmail.com>
2021-07-09 16:29:17 +0300

    add models to the admin panel

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from content.models import Tariff, Customer, Subscription
+
+admin.site.register(Tariff)
+admin.site.register(Customer)
+admin.site.register(Subscription)
