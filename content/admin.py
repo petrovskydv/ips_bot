46b0ae1f9a8f544ace9b6212f5389d00fb12abda
Денис Петровский <petrovsky.dv@gmail.com>
2021-07-14 11:48:35 +0300

    changed admin panel

--- content/admin.py
+++ content/admin.py
@@ -2,6 +2,29 @@ from django.contrib import admin
 
 from content.models import Tariff, Customer, Subscription
 
-admin.site.register(Tariff)
-admin.site.register(Customer)
 admin.site.register(Subscription)
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    search_fields = [
+        'login',
+    ]
+    list_display = [
+        'login',
+        'tg_chat_id',
+    ]
+    ordering = ['login']
+
+
+@admin.register(Tariff)
+class TariffAdmin(admin.ModelAdmin):
+    search_fields = [
+        'title',
+    ]
+    list_display = [
+        'title',
+        'tariff_type',
+        'cost',
+    ]
+    ordering = ['title']
